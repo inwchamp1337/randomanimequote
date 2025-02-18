0a93955f0401568691499afc02d9e3c5a95abd91
inwchamp1337 <ppasinb@gmail.com>
2025-02-18 21:32:46 +0700

    Initial Commit

---
+++ randomquote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomquote
+//
+//  Created by Csy on 18/2/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
